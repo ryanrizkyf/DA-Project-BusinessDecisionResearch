f4d4bd1829f6c449ba8e1ae4a3654cf402a3403f
Ryan <ryanrizkyf@gmail.com>
2021-02-28 11:55:18 +0700

    edit commit

--- Project_BDR.py
+++ Project_BDR.py
@@ -14,3 +14,21 @@ print(df.head())
 
 print('\nInfo dataset:')
 print(df.info())
+
+# Data Cleansing
+# Dua kolom yang menunjukkan terjadinya transaksi tidak bertipe datetime,
+# maka ubahlah kedua kolom tersebut ke tipe data datetime.
+# Kemudian cetaklah kembali 5 data teratas dari dataframe df dan juga tipe data masing-masing kolomnya.
+
+# Kolom First_Transaction
+df['First_Transaction'] = pd.to_datetime(
+    df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+# Kolom Last_Transaction
+df['Last_Transaction'] = pd.to_datetime(
+    df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+
+print('Lima data teratas:')
+print(df.head())
+
+print('\nInfo dataset:')
+print(df.info())
